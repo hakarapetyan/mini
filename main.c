1c1ef92c147a83831399cc4d3b4f2e6c69785193
hakarapetyan <hamestk77@gmail.com>
2024-12-12 01:39:48 +0400

    Add files via upload
    updated check_key,chang_shlvl functions , sareri qaminnnnnnnnnnnnnnnnnnnnn~~~~~~~~~~~~~~

--- main.c
+++ main.c
@@ -1,109 +1,52 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hakarape <hakarape@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/15 19:32:14 by ashahbaz          #+#    #+#             */
-/*   Updated: 2024/12/11 20:51:30 by hakarape         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
-#include "./include/minishell.h"
-
-int status = 0;
-
-static int	check_redir_errors(t_shell *shell)
+// Increment SHLVL
+char *inc_shlvl(t_shell *shell)
 {
-	t_token	*tkn;
+    char *value;
+    int atoi;
+    char *itoa;
 
-	tkn = shell -> token;
-	if (!tkn)
-		return (-1);
-	if (tkn -> type == PIPE)
-	{
-		syntax_error(tkn -> value);
-			return (-1);
-	}
-	while (tkn)
-	 {
-		if (is_redirection(tkn -> type) && ((tkn -> next && is_redirection(tkn -> next -> type)) ||
-			 !(tkn -> next) || (tkn -> next && tkn -> next -> type == PIPE)))
-		{
-			if (tkn -> next)
-			syntax_error(tkn -> next -> value);
-			else if (!(tkn -> next))
-				syntax_error("newline");
-			return (-1);
-		}
-		if (tkn -> type == PIPE && ((tkn -> next && tkn -> next -> type == PIPE) ||
-			 !(tkn -> next) || (tkn -> next && tkn -> next -> type == PIPE)))
-		{
-			if (tkn -> next)
-			syntax_error(tkn -> next -> value);
-			else if (!(tkn -> next))
-				syntax_error("newline");
-			return (-1);
-		}
-		 tkn = tkn -> next;
-	}
+    value = get_value(shell, "SHLVL=");
+    printf("value=%s\n", value);
+    atoi = ft_atoi(value);
+    itoa = ft_itoa(atoi + 1);
+    return itoa;
+}
+
+// Change SHLVL
+int check_key(env_list *list, const char *key)
+{
+	env_list *env;
+
+	env = list;
+    if (ft_strcmp(env->key, key) == 0)
+        return (1);
+    env = env->next;
 	return (0);
 }
 
-int	main(int argc, char **argv, char **env)
+void chang_shlvl(t_shell *shell)
 {
-	t_shell	shell;
+	env_list *list;
+	char	*shlvl;
 
-	(void)argc;
-	(void)argv;
-	(void)env;
-	//init_shell(&shell);
-	rl_catch_signals = 0;
-		get_environment(&shell, env);
-		chang_shlvl(&shell);
-	while (1)
+	shlvl = inc_shlvl(shell);
+	list = shell->env;
+	if (!ft_strcmp(shell -> input, "./minishell"))
 	{
-		// rl_replace_line("", 0);
-		// rl_on_new_line();
-		signals(INTERACTIVE);
-		shell.input = readline("\033[1;35m.minishell \033[0m");
-		//	system("leaks minishell");
-		if (!shell.input)
-			break;//free anel
-		// error(READLINE_ERR, &shell);
-		if (shell.input[0] != '\0')
-			add_history(shell.input);
-		lexical_analyzer(&shell);
-		if (check_redir_errors(&shell) >= 0)
+		while(list)
 		{
-				//print_tokens(&shell);
-			if (create_commands(&shell) >= 0)
+			if (check_key(list, "SHLVL=") == 1)
 			{
-				//free_tokens(shell.token);
-				//print_commands(&shell);
-				if (prepare_redirections(&shell) >= 0)
-				{
-					execute(&shell);
-					
-				}
-					dup2(shell.command -> stdin_original, STDIN_FILENO);
-					dup2(shell.command -> stdout_original, STDOUT_FILENO);
-				
+				free(list->value);
+				list->value=ft_strdup(shlvl);
+				free(shlvl);
+				return;
 			}
+			list = list->next;
 		}
-		free_shell(&shell);
-		//system("leaks minishell");
 	}
-	free_env(shell.env);
-	shell.env = NULL;
-	free_env(shell.exp);
-	shell.exp = NULL;
-	free_shell(&shell);
-	//system("leaks minishell");
-	// printf("%lld\n",ft_atoi("999"));
-	// printf("%lld\n",ft_atoi("1"));
-	// printf("%lld\n",ft_atoi("0"));
-	// printf("%lld\n",ft_atoi("9223372036854775809"));
-	return (0);
+
+	free(shlvl);
+	return ;
 }
